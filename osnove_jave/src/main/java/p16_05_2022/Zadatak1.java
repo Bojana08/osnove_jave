fbf859d934b28ccafd6906b91b8d373958847609
Bojana Radojkovic <bojana.08@gmail.com>
2022-05-17 09:01:33 +0200

    p16_05_2022 Zadatak 1

---
+++ osnove_jave/src/main/java/p16_05_2022/Zadatak1.java
@@ -0,0 +1,18 @@
+package p16_05_2022;
+
+public class Zadatak1 {
+    public static void main(String[] args) {
+
+//        1. Zadatak
+//        Napisati metodu koja stampa ime i prezime i u glavnom programu pozvati metodu na izvrsenje 5 puta.
+
+        MyMethod();
+        MyMethod();
+        MyMethod();
+        MyMethod();
+        MyMethod();
+    }
+    static void MyMethod() {
+        System.out.println("Bojana Radojkovic");
+    }
+}
