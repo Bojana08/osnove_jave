6d00499ebd379a758f4ab7a20a254ae7bb8f14a9
Bojana Radojkovic <bojana.08@gmail.com>
2022-05-17 09:03:14 +0200

    p16_05_2022 Zadatak2

---
+++ osnove_jave/src/main/java/p16_05_2022/Zadatak2.java
@@ -0,0 +1,27 @@
+package p16_05_2022;
+
+public class Zadatak2 {
+    public static void main(String[] args) {
+//
+//        2. Zadatak
+//        Napisati dve metode, jedna metoda sluzi za stampanje imena a druga sluzi za stampanje prezimena.
+//        U glavnom programu pozvati metode na izvrsenje tako da se 5 puta ispise ime i prezime u istom redu.
+
+        Ime();
+        Prezime();
+        Ime();
+        Prezime();
+        Ime();
+        Prezime();
+        Ime();
+        Prezime();
+        Ime();
+        Prezime();
+    }
+    static void Ime() {
+        System.out.print("Bojana ");
+    }
+    static void Prezime() {
+        System.out.println("Radojkovic");
+    }
+}
